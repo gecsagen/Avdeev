4f81bb248f4f35a187826bd0add4a05230a684fa
Karmickle <gjfjfhhhf@gmail.com>
2021-12-09 08:45:19 -0800

    final

--- Avdeev/settings.py
+++ Avdeev/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'Avdeev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'base_avdeev',
+        'USER': 'user_avdeev',
+        'PASSWORD': '12345',
     }
 }
 
